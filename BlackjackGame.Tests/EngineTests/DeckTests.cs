307a1f9bb71be7e462a724445d9fc37c8ecbcf8c
maviakazi12 <mavia.kazi@myob.com>
2025-04-20 23:37:00 +1000

    created the deck

---
+++ BlackjackGame.Tests/EngineTests/DeckTests.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using BlackjackGame.Engine;
+
+
+
+namespace BlackjackGameTests.EngineTests
+{
+    public class DeckTests
+    {
+        [Fact]
+        public void Deck_Should_Contain_52_Cards()
+        {
+            // Arrange
+            var deck = new Deck();
+
+            // Act
+            var cardDeck = deck.deckOfCards;
+
+            // Assert
+            cardDeck.Should().HaveCount(52);
+        }
+
+        [Fact]
+        public void Deck_Should_Have_Unique_Cards()
+        {
+            // Arrange
+            var deck = new Deck();
+
+            // Act
+            var cardDeck = deck.deckOfCards;
+
+            // Assert
+            cardDeck.Select(card => (card.Suit, card.Rank)).Should().OnlyHaveUniqueItems();
+        }
+        [Fact]
+        public void Shuffled_Cards_Should_Not_Be_Same_As_Original_Cards()
+        {
+            // Arrange
+            var deck = new Deck();
+
+            // Act
+            var shuffledCards = deck.shuffle(cards);
+
+            // Assert
+            shuffledCards.Should().NotBe(deck);
+        }
+    }
+
+}
