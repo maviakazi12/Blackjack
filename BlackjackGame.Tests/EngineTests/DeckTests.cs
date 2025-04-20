187900c3b8bb4de95971c0b74e073220f68959b9
maviakazi12 <mavia.kazi@myob.com>
2025-04-21 00:05:34 +1000

    add shuffling tests and functionality

--- BlackjackGame.Tests/EngineTests/DeckTests.cs
+++ BlackjackGame.Tests/EngineTests/DeckTests.cs
@@ -37,18 +37,50 @@ namespace BlackjackGameTests.EngineTests
             // Assert
             cardDeck.Select(card => (card.Suit, card.Rank)).Should().OnlyHaveUniqueItems();
         }
+
         [Fact]
         public void Shuffled_Cards_Should_Not_Be_Same_As_Original_Cards()
         {
             // Arrange
             var deck = new Deck();
+            var originalDeck = deck.deckOfCards.ToList();
 
             // Act
-            var shuffledCards = deck.shuffle(cards);
+            deck.Shuffle();
+            var shuffledDeck = deck.deckOfCards.ToList();
 
             // Assert
-            shuffledCards.Should().NotBe(deck);
+            shuffledDeck.Should().NotEqual(originalDeck);
         }
+
+        [Fact]
+        public void Shuffled_Cards_Should_Contain_52_Cards()
+        {
+            // Arrange
+            var deck = new Deck();
+
+            // Act
+            deck.Shuffle();
+            var shuffledDeck = deck.deckOfCards.ToList();
+
+            // Assert
+            shuffledDeck.Should().HaveCount(52);
+        }
+
+    [Fact]
+        public void Shuffled_Deck_Should_Have_Unique_Cards()
+        {
+            // Arrange
+            var deck = new Deck();
+
+            // Act
+            deck.Shuffle();
+            var shuffledDeck = deck.deckOfCards.ToList();
+
+            // Assert
+            shuffledDeck.Select(card => (card.Suit, card.Rank)).Should().OnlyHaveUniqueItems();
+        }
+
     }
 
 }
