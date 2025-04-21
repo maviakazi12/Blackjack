056df20cc3b0802a0e645d4fd267c045c0fde09a
maviakazi12 <mavia.kazi@myob.com>
2025-04-21 15:51:57 +1000

    interface: add IDeck interface

--- BlackjackGame/Interfaces/IDeck.cs
+++ BlackjackGame/Interfaces/IDeck.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlackjackGame.Models;
 
 namespace BlackjackGame.Interfaces
 {
     public interface IDeck
     {
-        
+        public List<Card> deckOfCards { get; }
+        public List<Card> drawnCard { get; }
+        void InitializeDeck();
+        void Shuffle();
     }
 }
