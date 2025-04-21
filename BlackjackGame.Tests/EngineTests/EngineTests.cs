eb03f0f9634d448f2fad19b9a00747bc6ee0b3ac
maviakazi12 <mavia.kazi@myob.com>
2025-04-21 15:46:55 +1000

    test: write first engine test

--- BlackjackGame.Tests/EngineTests/EngineTests.cs
+++ BlackjackGame.Tests/EngineTests/EngineTests.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using BlackjackGame.Engine;
+using Moq;
 
 namespace BlackjackTests.EngineTests
 {
     public class EngineTests
     {
         [Fact]
-        public void Test1()
+        public void StartGame_Should_Initialize_The_Deck()
         {
-            Assert.True(true);
+            // Arrange
+            var mockDeck = new Mock<IDeck>();
+            
+            // Act
+            Engine.StartGame(mockDeck.Object);
+
+            // Assert
+            mockDeck.Verify(deck=> deck.InitializeDeck(), Times.Once);
         }
     }
 }
