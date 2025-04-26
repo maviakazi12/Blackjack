b3af386f7c4efdf6a619c2b283583c26bcbee5e0
maviakazi12 <mavia.kazi@myob.com>
2025-04-26 20:37:07 +1000

    test: add automock to simplify mocking in gamecontroller

--- BlackjackGame.Tests/EngineTests/GameControllerTests.cs
+++ BlackjackGame.Tests/EngineTests/GameControllerTests.cs
@@ -3,156 +3,164 @@ using BlackjackGame.Interfaces;
 using BlackjackGame.Enums;
 using BlackjackGame.Models;
 using Moq;
+using Moq.AutoMock;
 using FluentAssertions;
 
 namespace BlackjackTests.EngineTests
 {
     public class GameControllerTests
     {
+        private (GameController controller, AutoMocker mocker) CreateGameController(int initialCards = 2)
+        {
+            var mocker = new AutoMocker();
+
+            var controller = new GameController(
+                mocker.Get<IDeck>(),
+                mocker.Get<IPlayer>(),
+                mocker.Get<IPlayer>(),
+                initialCards,
+                mocker.Get<IIO>(),
+                mocker.Get<IScoringService>()
+            );
+
+            return (controller, mocker);
+        }
+
         [Fact]
         public void StartGame_Should_Call_InitializeDeck_Method()
         {
             // Arrange
-            var mockDeck = new Mock<IDeck>();
-            GameController gameController = new GameController(mockDeck.Object, Mock.Of<IPlayer>(), Mock.Of<IPlayer>(), 2, Mock.Of<IIO>(), Mock.Of<IScoringService>());
+            var (controller, mocker) = CreateGameController();
 
             // Act
-            gameController.Run();
+            controller.Run();
 
             // Assert
-            mockDeck.Verify(deck => deck.InitializeDeck(), Times.Once);
-
+            mocker.GetMock<IDeck>().Verify(deck => deck.InitializeDeck(), Times.Once);
         }
 
         [Fact]
         public void StartGame_Should_Call_Shuffle_Method()
         {
             // Arrange
-            var mockDeck = new Mock<IDeck>();
-            GameController gameController = new GameController(mockDeck.Object, Mock.Of<IPlayer>(), Mock.Of<IPlayer>(), 2, Mock.Of<IIO>(), Mock.Of<IScoringService>());
+            var (controller, mocker) = CreateGameController();
 
             // Act
-            gameController.Run();
+            controller.Run();
 
             // Assert
-            mockDeck.Verify(deck => deck.Shuffle(), Times.Once);
-
+            mocker.GetMock<IDeck>().Verify(deck => deck.Shuffle(), Times.Once);
         }
 
         [Fact]
         public void StartGame_Should_Call_Draw_2_Times_For_Player_And_Dealer()
         {
             // Arrange
-            var mockDeck = new Mock<IDeck>();
-            GameController gameController = new GameController(mockDeck.Object, Mock.Of<IPlayer>(), Mock.Of<IPlayer>(), 2, Mock.Of<IIO>(), Mock.Of<IScoringService>());
+            var (controller, mocker) = CreateGameController();
 
             // Act
-            gameController.Run();
+            controller.Run();
 
             // Assert
-            mockDeck.Verify(deck => deck.Draw(2), Times.Exactly(2));
+            mocker.GetMock<IDeck>().Verify(deck => deck.Draw(2), Times.Exactly(2));
         }
 
         [Fact]
         public void StartGame_Should_Deal_n_Number_Of_Cards()
         {
             // Arrange
-            var mockDeck = new Mock<IDeck>();
-            GameController gameController = new GameController(mockDeck.Object, Mock.Of<IPlayer>(), Mock.Of<IPlayer>(), 4, Mock.Of<IIO>(), Mock.Of<IScoringService>());
+            var (controller, mocker) = CreateGameController(4);
 
             // Act
-            gameController.Run();
+            controller.Run();
 
             // Assert
-            mockDeck.Verify(deck => deck.Draw(4), Times.Exactly(2));
+            mocker.GetMock<IDeck>().Verify(deck => deck.Draw(4), Times.Exactly(2));
         }
 
         [Fact]
         public void Constructor_Should_Throw_If_Deck_Is_Null()
         {
+            // Act & Assert
             Assert.Throws<ArgumentNullException>(() =>
-            new GameController(null, Mock.Of<IPlayer>(), Mock.Of<IPlayer>(), 2, Mock.Of<IIO>(), Mock.Of<IScoringService>()));
+                new GameController(
+                    null,
+                    Mock.Of<IPlayer>(),
+                    Mock.Of<IPlayer>(),
+                    2,
+                    Mock.Of<IIO>(),
+                    Mock.Of<IScoringService>())
+            );
         }
 
         [Fact]
         public void Run_Should_Call_ReceiveCards_On_Player_And_Dealer_With_Deck_DrawnCards()
         {
             // Arrange
-            var mockDeck = new Mock<IDeck>();
-            var mockPlayer = new Mock<IPlayer>();
-            var mockDealer = new Mock<IPlayer>();
-            GameController gameController = new GameController(mockDeck.Object, mockPlayer.Object, mockDealer.Object, 4, Mock.Of<IIO>(), Mock.Of<IScoringService>());
+            var (controller, mocker) = CreateGameController(4);
+
             var drawnCards = new List<Card>
-            { new Card(Suit.heart, Rank.ace),
+            {
+                new Card(Suit.heart, Rank.ace),
                 new Card(Suit.spade, Rank.ten)
             };
-            mockDeck.Setup(deck => deck.drawnCards).Returns(drawnCards);
+
+            mocker.GetMock<IDeck>().Setup(deck => deck.drawnCards).Returns(drawnCards);
 
             // Act
-            gameController.Run();
+            controller.Run();
 
-            //Assert
-            mockPlayer.Verify(player => player.ReceiveCards(mockDeck.Object.drawnCards));
-            mockDealer.Verify(dealer => dealer.ReceiveCards(mockDeck.Object.drawnCards));
+            // Assert
+            mocker.GetMock<IPlayer>().Verify(player => player.ReceiveCards(drawnCards));
+            mocker.GetMock<IPlayer>().Verify(dealer => dealer.ReceiveCards(drawnCards));
         }
 
         [Fact]
         public void PlayPlayerTurn_Should_Call_ReceiveCards_On_Player_They_Choose_Hit()
         {
-            //Arrange
-            var mockIO = new Mock<IIO>();
-            var mockDeck = new Mock<IDeck>();
-            var mockPlayer = new Mock<IPlayer>();
-            var mockDealer = new Mock<IPlayer>();
-            GameController gameController = new GameController(mockDeck.Object, mockPlayer.Object, mockDealer.Object, 2, mockIO.Object, Mock.Of<IScoringService>());
-            var drawnCard = new List<Card>
-            { new Card(Suit.heart, Rank.ace)};
+            // Arrange
+            var (controller, mocker) = CreateGameController();
 
-            mockDeck.Setup(d => d.drawnCards).Returns(drawnCard);
-            mockIO.Setup(a => a.GetPlayerChoice()).Returns("hit");
+            var drawnCard = new List<Card> { new Card(Suit.heart, Rank.ace) };
 
-            //Act
-            gameController.PlayPlayerTurn();
+            mocker.GetMock<IDeck>().Setup(deck => deck.drawnCards).Returns(drawnCard);
+            mocker.GetMock<IIO>().Setup(io => io.GetPlayerChoice()).Returns("hit");
 
-            //Assert
-            mockPlayer.Verify(player => player.ReceiveCards(drawnCard), Times.Once);
+            // Act
+            controller.PlayPlayerTurn();
+
+            // Assert
+            mocker.GetMock<IPlayer>().Verify(player => player.ReceiveCards(drawnCard), Times.Once);
         }
 
         [Fact]
         public void PlayPlayerTurn_Should_Not_Draw_Card_When_Player_Chooses_Stay()
         {
-            //Arrange
-            var mockIO = new Mock<IIO>();
-            var mockDeck = new Mock<IDeck>();
-            var mockPlayer = new Mock<IPlayer>();
-            var mockDealer = new Mock<IPlayer>();
-            GameController gameController = new GameController(mockDeck.Object, mockPlayer.Object, mockDealer.Object, 2, mockIO.Object, Mock.Of<IScoringService>());
-            mockIO.Setup(a => a.GetPlayerChoice()).Returns("stay");
+            // Arrange
+            var (controller, mocker) = CreateGameController();
 
-            //Act
-            gameController.PlayPlayerTurn();
+            mocker.GetMock<IIO>().Setup(io => io.GetPlayerChoice()).Returns("stay");
 
-            //Assert
-            mockPlayer.Verify(player => player.ReceiveCards(It.IsAny<List<Card>>()), Times.Never());
+            // Act
+            controller.PlayPlayerTurn();
+
+            // Assert
+            mocker.GetMock<IPlayer>().Verify(player => player.ReceiveCards(It.IsAny<List<Card>>()), Times.Never());
         }
 
         [Fact]
         public void PlayPlayerTurn_Should_Set_GameState_To_Dealer_After_Hit_When_Not_Bust_Or_Win()
         {
-            //Arrange
-            var mockIO = new Mock<IIO>();
-            var mockDeck = new Mock<IDeck>();
-            var mockPlayer = new Mock<IPlayer>();
-            var mockDealer = new Mock<IPlayer>();
-            GameController gameController = new GameController(mockDeck.Object, mockPlayer.Object, mockDealer.Object, 2, mockIO.Object, Mock.Of<IScoringService>());
-            mockIO.Setup(a => a.GetPlayerChoice()).Returns("hit");
+            // Arrange
+            var (controller, mocker) = CreateGameController();
 
-            //Act
-            gameController.PlayPlayerTurn();
+            mocker.GetMock<IIO>().Setup(io => io.GetPlayerChoice()).Returns("hit");
 
-            //Assert
-            gameController.GameState.CurrentTurn.Should().Be(PlayerType.Dealer);
+            // Act
+            controller.PlayPlayerTurn();
+
+            // Assert
+            controller.GameState.CurrentTurn.Should().Be(PlayerType.Dealer);
         }
-
     }
 }
