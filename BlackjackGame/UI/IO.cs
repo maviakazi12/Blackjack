95e48b0ed535cd40821c11003390bad997eaf41e
maviakazi12 <mavia.kazi@myob.com>
2025-04-28 14:51:50 +1000

    refactor: abstracted out display formatting logic to a method to follow DRY rules

--- BlackjackGame/UI/IO.cs
+++ BlackjackGame/UI/IO.cs
@@ -35,19 +35,44 @@ namespace BlackjackGame.UI
                 Console.WriteLine(PromptMessages.DealerWins);
         }
 
+        public void DrawGame()
+        {
+            Console.WriteLine(PromptMessages.Draw);
+        }
+        public void DisplayDealerChoice(string dealerChoice)
+        {
+            if (dealerChoice == "hit")
+            {
+                Console.WriteLine(PromptMessages.DealerHit);
+            }
+            Console.WriteLine(PromptMessages.DealerStay);
+        }
+
         public void DisplayPlayerHand(List<Card> playerHand)
+        {
+            FormatDisplay(PlayerType.Player.ToString(), playerHand);
+        }
+
+        public void DisplayDealerHand(List<Card> dealerHand)
+        {
+            FormatDisplay(PlayerType.Dealer.ToString(), dealerHand);
+        }
+
+        public void FormatDisplay(string player, List<Card> hand)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8; // Enable emojis
 
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"\n{player} Hand:");
+            Console.ResetColor();
+
             // Header
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("\nYour Hand:");
             Console.WriteLine($"{"Rank",-6} | {"Suit",-10}");
             Console.WriteLine(new string('-', 24));
             Console.ResetColor();
 
-            // Loop through cards
-            foreach (var card in playerHand)
+            foreach (var card in hand)
             {
                 string emoji = "";
 
@@ -78,55 +103,7 @@ namespace BlackjackGame.UI
                 Console.WriteLine($"{card.Rank,-6} | {card.Suit,-10} {emoji}");
             }
 
-            Console.ResetColor(); // Reset after full hand is printed
-        }
-
-        public void DisplayDealerHand(List<Card> dealerHand)
-        {
-            Console.OutputEncoding = System.Text.Encoding.UTF8; // Enable emojis
-
-    Console.ForegroundColor = ConsoleColor.DarkCyan;
-    Console.WriteLine("\nDealer's Hand:");
-    Console.ResetColor();
-
-    // Header
-    Console.ForegroundColor = ConsoleColor.Magenta;
-    Console.WriteLine($"{"Rank",-6} | {"Suit",-10}");
-    Console.WriteLine(new string('-', 24));
-    Console.ResetColor();
-
-    foreach (var card in dealerHand)
-    {
-        string emoji = "";
-
-        switch (card.Suit)
-        {
-            case Suit.heart:
-                emoji = "♥️";
-                Console.ForegroundColor = ConsoleColor.Red;
-                break;
-            case Suit.diamond:
-                emoji = "♦️";
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                break;
-            case Suit.club:
-                emoji = "♣️";
-                Console.ForegroundColor = ConsoleColor.Green;
-                break;
-            case Suit.spade:
-                emoji = "♠️";
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                break;
-            default:
-                emoji = "";
-                Console.ForegroundColor = ConsoleColor.Gray;
-                break;
-        }
-
-        Console.WriteLine($"{card.Rank,-6} | {card.Suit,-10} {emoji}");
-    }
-
-    Console.ResetColor();
+            Console.ResetColor();
         }
     }
 }
