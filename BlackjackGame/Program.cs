0f160191a7170bba299f88c96d669904e9b96cd4
maviakazi12 <mavia.kazi@myob.com>
2025-05-08 13:39:37 +1000

    cleanup: cleaned and removed unnecessary code/files

--- BlackjackGame/Program.cs
+++ BlackjackGame/Program.cs
@@ -1,7 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BlackjackGame.UI;
 using BlackjackGame.Engine;
+using BlackjackGame.Services;
 
-Console.WriteLine("Hello, World!");
-// var deck = new Deck();
-// var gameController = new GameController(deck);
-// engine.StartGame();
+var deck = new Deck();
+var player = new Player();
+var dealer = new Dealer();
+var inputOutput = new IO();
+var scoring = new ScoringService();
+
+// Initialize GameController
+var gameController = new GameController(
+    deck,
+    player,
+    dealer,
+    2,            // initial cards to deal
+    inputOutput,
+    scoring
+);
+
+// Start Game
+gameController.Run();
+
